17efb85fd98a27d642b99270da33f963038dc465
Chiemeka <chi_aubrey@live.com>
2014-12-15 12:03:47 +0100

    working version 1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,32 +8,32 @@ shoppingCart.controller('ShoppingCartCtrl', function($scope) {
   $scope.carts = [
     {
       "img": "img/01.jpg",
-      "text": "What kind of jobs do software engineers who earn &#36;500k per year do&#63;",
+      "text": "What kind of jobs do software engineers who earn $500k per year do?",
       "link": "http://www.quora.com/What-kind-of-jobs-do-software-engineers-who-earn-500k-per-year-do"
     },
     {
       "img": "img/02.jpg",
-      "text": "What is a coder's worst nightmare&#63;",
+      "text": "What is a coder's worst nightmare?",
       "link": "http://www.quora.com/What-is-a-coders-worst-nightmare"
     },
     {
       "img": "img/03.jpg",
-      "text": "Who is the unluckiest physicist ever&#63;",
+      "text": "Who is the unluckiest physicist ever?",
       "link": "http://www.quora.com/Who-is-the-unluckiest-physicist-ever"
     },
     {
       "img": "img/04.jpg",
-      "text": "What are valuable skills that many young people are losing&#63;",
+      "text": "What are valuable skills that many young people are losing?",
       "link": "http://www.quora.com/What-are-valuable-skills-that-many-young-people-are-losing"
     },
     {
       "img": "img/05.jpg",
-      "text": "What are some interesting facts about working at Google&#63;",
+      "text": "What are some interesting facts about working at Google?",
       "link": "http://www.quora.com/What-are-some-interesting-facts-about-working-at-Google"
     },
     {
       "img": "img/06.jpg",
-      "text": "What are the stupidest decisions ever made in Silicon Valley&#63;",
+      "text": "What are the stupidest decisions ever made in Silicon Valley?",
       "link": "http://www.quora.com/What-are-the-stupidest-decisions-ever-made-in-Silicon-Valley"
     }
   ];
